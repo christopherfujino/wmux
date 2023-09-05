df4ff365e0213c11e91c50bb83efbf66b5d69c52
Christopher Fujino <christopherfujino@gmail.com>
2023-09-04 19:38:02 -0700

    wip spawn process

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,40 @@
-#include <iostream>
+#include <stdio.h>
+#include <windows.h>
 
 int main() {
-  std::cout << "Hello, world!\n";
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+
+  ZeroMemory(&si, sizeof(si));
+  si.cb = sizeof(si);
+  ZeroMemory(&pi, sizeof(pi));
+
+  char command[] = "ls";
+
+  if (!CreateProcess(NULL,    // lpApplicationName
+                     command, // lpCommandLine
+                     NULL,    // lpProcessAttributes
+                     NULL,    // lpThreadAttributes
+                     TRUE,   // bInheritHandles
+                     0,       // dwCreationFlags (no creation flags)
+                     NULL,    // lpEnvironment (use parent's env)
+                     NULL,    // lpCurrentDirectory (use parent)
+                     &si,     // lpStartupInfo
+                     &pi      // lpProcessInformation
+                     )) {
+    fprintf(stderr, "CreateProcess call failed (%ld)\n", GetLastError());
+    return 1;
+  }
+
+  printf("Waiting for child process...\n");
+
+  // Wait until process exits
+  WaitForSingleObject(pi.hProcess, INFINITE);
+
+  printf("Process exited!\n");
+  // Close process and thread handles.
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+
+  return 0;
 }
